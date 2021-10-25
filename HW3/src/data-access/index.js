802f8c7f30063f268ab0b5db083c73f6c9a5dd3c
Nazarkon <nazarmontsibovych@gmail.com>
2021-10-25 03:20:00 +0300

    final result

---
+++ HW3/src/data-access/index.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('postgres://mfoyljio:q4s21Kukr0nnzBUjs2OWSbRUNlTW_JwJ@hattie.db.elephantsql.com/mfoyljio');
+sequelize.authenticate();
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+const user = require('../models/user')(sequelize);
+
+module.exports = {
+    sequelize,
+    user
+};
---
+++ HW3/src/models/user.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+
+
+module.exports = function (sequelize) {
+    return sequelize.define('users', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false
+        },
+        login: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        age: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        isDeleted: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false
+        }
+    }, {
+        freezeTableName: true
+    });
+};
